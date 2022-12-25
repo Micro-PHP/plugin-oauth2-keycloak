f49847ab1d299572fec853b164c7d24424369f1a
Stanislau Komar <stanislau_komar@epam.com>
2022-12-25 04:33:19 +0400

    v1.0

---
+++ src/Configuration/Provider/ProviderConfiguration.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Micro framework package.
+ *
+ * (c) Stanislau Komar <head.trackingsoft@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Micro\Plugin\OAuth2\Client\Keycloak\Configuration\Provider;
+
+use Micro\Plugin\OAuth2\Client\Configuration\Provider\OAuth2ClientProviderConfiguration;
+use Micro\Plugin\OAuth2\Client\Keycloak\Provider\KeycloakProviderConfigurationInterface;
+
+/**
+ * @author Stanislau Komar <head.trackingsoft@gmail.com>
+ */
+class ProviderConfiguration extends OAuth2ClientProviderConfiguration implements KeycloakProviderConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getRealm(): string
+    {
+        return 'micro';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getScopesDefault(): array
+    {
+        return ['email', 'profile'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getScopesSeparator(): string
+    {
+        return ' ';
+    }
+
+    public function getSecurityProvider(): string|null
+    {
+        return null;
+    }
+}
